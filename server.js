a7c3b0bd14eb1482d83832c535e9541f0bb2b4b8
HannahYudkin <hannahyudkin@gmail.com>
2020-03-07 11:41:14 -0500

    Created server.

---
+++ server.js
@@ -0,0 +1,43 @@
+// Dependencies
+// =============================================================
+const express = require("express");
+const path = require("path");
+// const tables = require("tables");
+// const waitlist = require("waitlist");
+
+
+// Sets up the Express App
+// =============================================================
+const app = express();
+const PORT = process.env.PORT || 6666;
+
+// Sets up the Express app to handle data parsing
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+
+// Routes
+app.get("/", function(req, res) {
+    res.sendFile(path.join(__dirname, "./index.html"));
+});
+
+// app.get("/reserve", function(req, res) {
+//   res.sendFile(`${__dirname}/reserve.html`);
+// });
+
+// app.get("/tables", function(req, res) {
+//   res.sendFile(`${__dirname}/tables.html`);
+// });
+
+// app.get("/api/tables", function(req, res) {
+//     return res.json(tables);
+//   });
+
+//   app.get("/api/waitlist", function(req, res) {
+//     return res.json(waitlist);
+//   });
+
+
+app.listen(PORT, function() {
+    console.log("WOrking")
+})
