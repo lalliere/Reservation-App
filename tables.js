a8033e1523240a5a01d68940783dcb27fb8001c0
lalliere <emilylallier@yahoo.com>
2020-03-07 13:29:04 -0500

    removed test data from tables and waitlist arrays

--- tables.js
+++ tables.js
@@ -1,16 +1,5 @@
 const tables = [
-    {
-        "customerName": "asdf",
-        "phoneNumber": "asdf",
-        "customerEmail": "sadf",
-        "customerID": "asdfdd"
-    },
-    {
-        "customerName": "asdf",
-        "phoneNumber": "asdf",
-        "customerEmail": "sadf",
-        "customerID": "asdfdd"
-    }
+    
 ];
 
 module.exports = tables;
--- waitlist.js
+++ waitlist.js
@@ -1,16 +1,5 @@
 const waitlist = [
-    {
-        "customerName": "asdf",
-        "phoneNumber": "asdf",
-        "customerEmail": "sadf",
-        "customerID": "asdfdd"
-    },
-    {
-        "customerName": "asdf",
-        "phoneNumber": "asdf",
-        "customerEmail": "sadf",
-        "customerID": "asdfdd"
-    }
+    
 ];
 
 module.exports = waitlist;
